833077d3ff8c9d260025f20c494eee003ae1e7ba
kalmai <zacharykolker93@gmail.com>
2023-07-30 19:44:23 -0400

    completed d2p2

--- advent_of_code/2022/day_2/src/main.rs
+++ advent_of_code/2022/day_2/src/main.rs
@@ -7,17 +7,18 @@ fn main() {
     let data: Vec<String> = read_to_string(file_name).unwrap().lines().map(String::from).collect();
     let mut score_totals: i32 = 0;
     // A = rock, B = paper, C = scissors | OPPONENT CHOICE
-    // X = rock, Y = paper, Z = scissors | MY CHOICE
+    // rock = 1, paper = 2, scissors = 3
+    //   |loss|draw|win
     //   |  X |  Y |  Z
-    // A |  3 |  6 |  0
-    // B |  0 |  3 |  6
-    // C |  6 |  0 |  3
+    // A |  3 |  1 |  2
+    // B |  1 |  2 |  3
+    // C |  2 |  3 |  1
     let choice_map: HashMap<&str, HashMap<&str, i32>> = HashMap::from(
         [
             ("A", HashMap::from([
-                ("X", 3 + 1),
-                ("Y", 6 + 2),
-                ("Z", 0 + 3)
+                ("X", 0 + 3), // win/loss value + choice needed to make that happen value
+                ("Y", 3 + 1),
+                ("Z", 6 + 2)
             ])),
             ("B", HashMap::from([
                 ("X", 0 + 1),
@@ -25,9 +26,9 @@ fn main() {
                 ("Z", 6 + 3)
             ])),
             ("C", HashMap::from([
-                ("X", 6 + 1),
-                ("Y", 0 + 2),
-                ("Z", 3 + 3)
+                ("X", 0 + 2),
+                ("Y", 3 + 3),
+                ("Z", 6 + 1)
             ]))
         ]
     );
@@ -36,9 +37,9 @@ fn main() {
             continue;
         };
         let choices: Vec<&str> = datum.split(" ").collect();
-        let my_choice = choice_map.get(choices[0]).unwrap();
-        let choices_sum = my_choice.get(choices[1]).unwrap();
-        score_totals += choices_sum;
+        let opponent_choice = choice_map.get(choices[0]).unwrap();
+        let my_choice = opponent_choice.get(choices[1]).unwrap();
+        score_totals += my_choice;
     }
     println!("{}", score_totals);
 }
