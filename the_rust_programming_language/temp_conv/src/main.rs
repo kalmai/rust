39b9823a564413374256d10a863f35921840f5e3
kalmai <leozverres@gmail.com>
2026-02-18 01:54:17 -0500

    wrapped up temp conv kata

--- the_rust_programming_language/temp_conv/src/main.rs
+++ the_rust_programming_language/temp_conv/src/main.rs
@@ -5,16 +5,26 @@ fn main() {
     let mut input = String::new();
 
     println!("follow format to convert from one temperature scale to the other:");
-    println!("<original-scale><value><desired-scale>");
-    println!("F68C");
+    println!("<value><desired-scale-case-insensitive>");
+    println!("68C converts 68 fahrenheit to 20 celsius for example");
     match io::stdin().read_line(&mut input) {
         Ok(_) => {}
         Err(error) => println!("error: {error}"),
     };
     let input = input.trim().to_string().to_lowercase();
     let values = input.split_at(input.len() - 1);
-    let num = u32::from_str(values.0).unwrap();
+    let num = f32::from_str(values.0).unwrap();
     let scale = values.1;
-    println!("{}", num);
-    println!("{}", scale)
+
+    match scale {
+        "c" => {
+            println!("{input} is {:.1}F", ((num - 32.0) / 1.8))
+        }
+        "f" => {
+            println!("{input} is {:.1}C", num * 1.8 + 32.0)
+        }
+        &_ => {
+            println!("scale not supported")
+        }
+    };
 }
